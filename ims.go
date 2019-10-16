5617a5cb5f8d0e74608b5360e126c6ae5be4de02
Morelatto <pmichilis@uolinc.com>
2019-10-16 10:58:00 -0300

    Added custom json marshal and unmarshal for IM custom fields. Updated go modules files.

--- ims.go
+++ ims.go
@@ -1,41 +1,50 @@
 package hpsm
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"reflect"
+	"strings"
 	"time"
 )
-import "github.com/google/go-querystring/query"
+
+import (
+	"github.com/fatih/structs"
+	"github.com/google/go-querystring/query"
+	"github.com/trivago/tgo/tcontainer"
+)
 
 // IM represents a Service Manager incident.
 type IM struct {
-	AffectedCI      string    `json:"AffectedCI"`
-	Area            string    `json:"Area"`
-	Assignee        string    `json:"Assignee"`
-	AssignmentGroup string    `json:"AssignmentGroup"`
-	Category        string    `json:"Category"`
-	ClosedBy        string    `json:"ClosedBy"`
-	ClosedTime      time.Time `json:"ClosedTime"`
-	ClosureCode     string    `json:"ClosureCode"`
-	Company         string    `json:"Company"`
-	Description     []string  `json:"Description"`
-	Impact          string    `json:"Impact"`
-	IncidentID      string    `json:"IncidentID"`
-	Location        string    `json:"Location"`
-	OpenTime        time.Time `json:"OpenTime"`
-	OpenedBy        string    `json:"OpenedBy"`
-	ProblemType     string    `json:"ProblemType"`
-	SLAAgreementID  int       `json:"SLAAgreementID"`
-	Service         string    `json:"Service"`
-	Solution        []string  `json:"Solution"`
-	Status          string    `json:"Status"`
-	Subarea         string    `json:"Subarea"`
-	TicketOwner     string    `json:"TicketOwner"`
-	Title           string    `json:"Title"`
-	UpdatedBy       string    `json:"UpdatedBy"`
-	UpdatedTime     time.Time `json:"UpdatedTime"`
-	Urgency         string    `json:"Urgency"`
+	AffectedCI      string                `json:"AffectedCI"`
+	Area            string                `json:"Area"`
+	Assignee        string                `json:"Assignee"`
+	AssignmentGroup string                `json:"AssignmentGroup"`
+	Category        string                `json:"Category"`
+	ClosedBy        string                `json:"ClosedBy"`
+	ClosedTime      time.Time             `json:"ClosedTime"`
+	ClosureCode     string                `json:"ClosureCode"`
+	Company         string                `json:"Company"`
+	Description     []string              `json:"Description"`
+	Impact          string                `json:"Impact"`
+	IncidentID      string                `json:"IncidentID"`
+	Location        string                `json:"Location"`
+	OpenTime        time.Time             `json:"OpenTime"`
+	OpenedBy        string                `json:"OpenedBy"`
+	ProblemType     string                `json:"ProblemType"`
+	SLAAgreementID  int                   `json:"SLAAgreementID"`
+	Service         string                `json:"Service"`
+	Solution        []string              `json:"Solution"`
+	Status          string                `json:"Status"`
+	Subarea         string                `json:"Subarea"`
+	TicketOwner     string                `json:"TicketOwner"`
+	Title           string                `json:"Title"`
+	UpdatedBy       string                `json:"UpdatedBy"`
+	UpdatedTime     time.Time             `json:"UpdatedTime"`
+	Urgency         string                `json:"Urgency"`
+	OtherFields     tcontainer.MarshalMap // Rest of the fields should go here.
 }
 
 // IMService handles IMs for the API.
@@ -120,4 +129,67 @@ func (s *IMService) Search(q string, options *SearchOptions) ([]IM, *http.Respon
 		res = append(res, im.Incident)
 	}
 	return res, resp, err
+}
+
+// MarshalJSON is a custom JSON marshal function for the IM struct.
+// It handles custom fields and maps those from / to "OtherFields" key.
+func (i *IM) MarshalJSON() ([]byte, error) {
+	m := structs.Map(i)
+	unknowns, okay := m["OtherFields"]
+	if okay {
+		// if other fields are present, shift all key value from unknown to a level up
+		for key, value := range unknowns.(tcontainer.MarshalMap) {
+			m[key] = value
+		}
+		delete(m, "OtherFields")
+	}
+	return json.Marshal(m)
+}
+
+// UnmarshalJSON is a custom JSON marshal function for the IM struct.
+// It handles custom fields and maps those from / to "OtherFields" key.
+func (i *IM) UnmarshalJSON(data []byte) error {
+
+	// Do the normal unmarshalling first
+	// Details for this way: http://choly.ca/post/go-json-marshalling/
+	type Alias IM
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(i),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	totalMap := tcontainer.NewMarshalMap()
+	err := json.Unmarshal(data, &totalMap)
+	if err != nil {
+		return err
+	}
+
+	t := reflect.TypeOf(*i)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tagDetail := field.Tag.Get("json")
+		if tagDetail == "" {
+			// ignore if there are no tags
+			continue
+		}
+		options := strings.Split(tagDetail, ",")
+
+		if len(options) == 0 {
+			return fmt.Errorf("no tags options found for %s", field.Name)
+		}
+		// the first one is the json tag
+		key := options[0]
+		if _, okay := totalMap.Value(key); okay {
+			delete(totalMap, key)
+		}
+
+	}
+	i = (*IM)(aux.Alias)
+	// all the tags found in the struct were removed. Whatever is left are unknowns to struct
+	i.OtherFields = totalMap
+	return nil
 }
